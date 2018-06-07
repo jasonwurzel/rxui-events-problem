abe38ebc9b806238905110e998c2ed56d77abb24
Michael Kreich <jaykay2k@hotmail.com>
2018-06-07 08:43:26 +0200

    basic example for the issue
    see https://github.com/reactiveui/ReactiveUI/issues/1668

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+	    public Class1()
+	    {
+		    UIElement button = new Button();
+		    var elementVisual = ElementCompositionPreview.GetElementVisual(button);
+
+		    button.Events().DragEnter.Subscribe();
+	    }
+    }
+}
