cb29a773e6dba44ade8efe450c1c03e4f7fa176f
Michael Kreich <michaelkreich@hotmail.com>
2019-09-10 15:48:16 +0200

    updated to newest rxui, error has changed but remains

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Hosting;
 
 namespace ClassLibrary1
 {
@@ -14,9 +9,8 @@ namespace ClassLibrary1
 	    public Class1()
 	    {
 		    UIElement button = new Button();
-		    var elementVisual = ElementCompositionPreview.GetElementVisual(button);
 
 		    button.Events().DragEnter.Subscribe();
-	    }
+        }
     }
 }
